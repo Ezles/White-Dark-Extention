4b639ad745da7341c5b301ec9467a75893063edc
Ezles <enzosery88@gmail.com>
2023-05-23 23:07:42 +0200

    feat: fix indentation

--- background.js
+++ background.js
@@ -1,13 +1,10 @@
 chrome.runtime.onInstalled.addListener(function() {
-
     chrome.storage.sync.set({ "darkMode": false });
-    });
+});
 
-    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-        if (message.darkMode !== undefined) {
-
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.darkMode !== undefined) {
         chrome.storage.sync.set({ "darkMode": message.darkMode });
-
         sendResponse({ success: true });
     }
 });
